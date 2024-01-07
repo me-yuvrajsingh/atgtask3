4f4098a94d44fd3cdad5059ca4831bb8c9c44d65
me-yuvrajsingh <90550128+me-yuvrajsingh@users.noreply.github.com>
2024-01-07 13:20:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let btn = document.querySelector(".btn")
-let loader = document.querySelector(".loader")
-let enable = document.querySelector(".enable")
-let disable = document.querySelector(".disable")
-let input = document.querySelector(".input")
-let toHide = [loader, enable]
-const chngAtt = [btn, input]
+let btn = document.querySelector(".btn");
+let loader = document.querySelector(".loader");
+let enable = document.querySelector(".enable");
+let disable = document.querySelector(".disable");
+let input = document.querySelector(".input");
+let toHide = [loader, enable];
+const chngAtt = [btn, input];
 const change = ()=>{
     if(input.value.length<4){
-        alert("Atleast 4 characters are required")
+        alert("Atleast 4 characters are required");
     }else{
         btn.style.width = 98+"px";
-        loader.classList.remove("none")
-        btn.style.cursor = "not-allowed"
+        loader.classList.remove("none");
+        btn.style.cursor = "not-allowed";
         setTimeout(() => {
-            btn.style.width = `var(--btn-w)`
+            btn.style.width = `var(--btn-w)`;
             btn.classList.add("btn-disabled");
             chngAtt.forEach((e)=>{e.setAttribute("disabled", true)});
-            toHide.map((e)=>{e.classList.add("none")})
-            disable.classList.remove("none")
-            input.style.color = `var(--disabled-txt-clr)`
+            toHide.map((e)=>{e.classList.add("none")});
+            disable.classList.remove("none");
+            input.style.color = `var(--disabled-txt-clr)`;
         }, 2000);
     }
 }
